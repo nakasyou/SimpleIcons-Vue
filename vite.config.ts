8d2f467e47b04bbd2e663151bc2e4153b1a426aa
nakasyou <syoutarou1103@outlook.jp>
2023-05-08 18:49:45 +0900

    fix name

--- vite.config.ts
+++ vite.config.ts
@@ -15,8 +15,8 @@ export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, "./src/index.ts"),
-      name: "hello-button",
-      fileName: (format) => `hello-button.${format}.js`,
+      name: "simpleicons-vue",
+      fileName: (format) => `simpleicons-vue.${format}.js`,
     },
     rollupOptions: {
       external: ["vue"],
